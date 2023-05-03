4732c682aed7842a2a6912d9a87d7a455e66f9bc
Vaibhav Shevne <112811407+vaibhzz101@users.noreply.github.com>
2023-05-03 15:33:57 +0530

    Add files via upload

--- middleware/logger.js
+++ middleware/logger.js
@@ -1,16 +1,31 @@
-const winston = require("winston")
+
 
 const MongoDB = require("winston-mongodb");
 
-const logger = winston.createLogger({
-    level: "error",
-    format: winston.format.json(),
-    transports: [
-        new MongoDB({
-            db: process.env.MONGO_URL,
-            options: {useUnifiedTopology:true},
-            collections: 'logs',
-        })
-    ],
-})
+// const logger = winston.createLogger({
+//     level: "error",
+//     format: winston.format.json(),
+//     transports: [
+//        MongoDB({
+//             db: process.env.MONGO_URL,
+//             options: {useUnifiedTopology:true},
+//             collections: 'logs',
+//         })
+//     ],
+// })
+const winston = require('winston');
+const { createLogger, transports } = winston;
+
+const logger = createLogger({
+  transports: [
+    new transports.Console(),
+    new transports.MongoDB({
+      level: 'error',
+      db: process.env.MONGO_URL,
+      options: { useUnifiedTopology: true },
+      collection: 'logs',
+      format: winston.format.combine(winston.format.timestamp(), winston.format.json()),
+    }),
+  ],
+});
 module.exports = logger
